b8a1b2fdbcf211616d349bf3e3ee9059175981a7
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2022-03-01 15:52:27 +0200

    inital

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts');
+});
+Route::get('/posts/{post}', function ($slug) {
+    $path = __DIR__."/../resources/posts/{$slug}.html";
+    if (! file_exists($path)){
+        return redirect("/");
+    }
+    $post = file_get_contents($path);
+    return view('post', [
+        "post"=>$post
+    ]);
+})->where("post","[A-z_\-]+");
+
+Route::get('/hello', function ()
+{
+    return "Hello World";
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
