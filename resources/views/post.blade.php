633c359fa0358dde5cac0c386171cf71e438e787
= <=>
2022-03-10 13:05:37 +0200

    Finish the work with file based posts

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/minu.css">
-    <title>Blog post</title>
-</head>
-<body>
-    <?= $post ?>
+<x-layout>
+    <article>
+        <h1>{{ $post->title }}</h1>
+        <div>
+            {!! $post->body !!}
+        </div>
+    </article>
     <a href="/">Go back</a>
-</body>
-</html>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,20 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/minu.css">
-    <title>Blog posts</title>
-</head>
-<body>
-    <?php foreach ($posts as $post) : ?>
-        <article>
-            <h1><?= $post->title; ?></h1>
-            <div>
-                <?= $post->body; ?>
-            </div>
-        </article>
-    <?php endforeach; ?>
-</body>
-</html>
+<x-layout>
+    @foreach($posts as $post)
+    <article>
+        <h1>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
+            </a>
+        </h1>
+        <div>
+            {{ $post->excerpt }}
+        </div>
+    </article>
+    @endforeach
+</x-layout>
