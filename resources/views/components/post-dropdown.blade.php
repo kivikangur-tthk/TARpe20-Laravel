fb28d3c565ffbc8799efde905b6e1b462f3293bc
= <=>
2022-03-31 09:07:02 +0300

    Cleanup from video 36

--- resources/views/components/post-dropdown.blade.php
+++ resources/views/components/post-dropdown.blade.php
@@ -7,7 +7,7 @@
     </div>
     {{-- links --}}
     <div x-show="show"
-         class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50"
+         class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50 overflow-auto max-h-52"
          style="display: none">
         {{ $slot }}
     </div>
